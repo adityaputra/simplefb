a7a48aeac563492857f5d50738fb8487b1a85960
aditya <adityaf.putra@gmail.com>
2015-11-12 07:30:46 +0700

    separating navbar, header, footer

---
+++ application/views/general/footer.php
@@ -0,0 +1,6 @@
+<!-- script references -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>public/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url();?>public/js/scripts.js"></script>
+</body>
+</html>
